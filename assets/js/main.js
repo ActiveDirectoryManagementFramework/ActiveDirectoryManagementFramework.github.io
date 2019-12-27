cec2ee239e99e4f095e2c9ffb2e5899d168d92d0
Friedrich Weinmann <friedrich.weinmann@powershell-stuttgart.de>
2019-12-27 17:10:18 +0100

    experimenting with navigation bar

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,19 +1,36 @@
 var sectionHeight = function() {
     var total    = $(window).height(),
         $section = $('section').css('height','auto');
-
+  
     if ($section.outerHeight(true) < total) {
-        var margin = $section.outerHeight(true) - $section.height();
-        $section.height(total - margin - 20);
+      var margin = $section.outerHeight(true) - $section.height();
+      $section.height(total - margin - 20);
     } else {
-        $section.css('height','auto');
+      $section.css('height','auto');
     }
-}
+  }
+  
+  $(window).resize(sectionHeight);
 
-$(window).resize(sectionHeight);
-
-$(function() {
+  $(function() {
     sectionHeight();
   
     $('img').on('load', sectionHeight);
+  })
+  
+  $(function() {
+    var results = null;
+    $.ajax({
+        type: 'GET',
+        url: 'nav.json',
+        dataType: 'json',
+        success: function(data) { results = data; },
+        async: false
+    });
+    $("nav p").innerHtml = "<a href='" + results.BackTo.Link + "'>" + results.BackTo.Label + "</a>";
+    results.fellows.each(function(){
+        $("nav ul").append("<li class='tag-h2'><a href='" + $(this).Link + "'>" + $(this).Label + "</a></li>");
+    })
+  
+    sectionHeight();
   });
