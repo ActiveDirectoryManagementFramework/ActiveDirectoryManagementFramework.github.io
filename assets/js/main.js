f25ad2782cf8e441fa8655b01dbcde6f30a259ad
Friedrich Weinmann <friedrich.weinmann@powershell-stuttgart.de>
2019-12-27 17:33:41 +0100

    fixing variable name

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,10 +10,10 @@ var sectionHeight = function() {
     }
 }
 var navigationBuilder = function(data) {
-    document.getElementById("backToParent").innerHtml = "<a href='" + results.backto.Link + "'>" + results.backto.Label + "</a>";
+    document.getElementById("backToParent").innerHtml = "<a href='" + data.backto.Link + "'>" + data.backto.Label + "</a>";
     var i;
-    for (i = 0; i < results.fellows.length; i++) {
-        $("nav ul").append("<li class='tag-h2'><a href='" + results.fellows[i].Link + "'>" + results.fellows[i].Label + "</a></li>");
+    for (i = 0; i < data.fellows.length; i++) {
+        $("nav ul").append("<li class='tag-h2'><a href='" + data.fellows[i].Link + "'>" + data.fellows[i].Label + "</a></li>");
     }
 
     sectionHeight();
