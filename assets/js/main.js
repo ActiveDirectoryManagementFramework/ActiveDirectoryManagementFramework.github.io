0a555f072720d6cf933903ffa2d9383cb39f364b
Friedrich Weinmann <friedrich.weinmann@powershell-stuttgart.de>
2019-12-27 19:23:56 +0100

    more experiments

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,12 +10,13 @@ var sectionHeight = function() {
     }
 }
 var navigationBuilder = function(data) {
-    var parentLink = document.getElementById("backToParent");
-    parentLink.innerHTML = "<a href='" + data.backto.Link + "'>Back to: " + data.backto.Label + "</a>";
+    document.getElementById("backToParent").innerHTML = "<a href='" + data.backto.Link + "'>Back to: " + data.backto.Label + "</a>";
+    document.getElementById("backToParent2").innerHTML = "<a href='" + data.backto.Link + "'>Back to: " + data.backto.Label + "</a>";
 
     var i;
     for (i = 0; i < data.fellows.length; i++) {
         $("nav ul").append("<li class='tag-h2'><a href='" + data.fellows[i].Link + "'>" + data.fellows[i].Label + "</a></li>");
+        $("section ul").append("<li class='tag-h2'><a href='" + data.fellows[i].Link + "'>" + data.fellows[i].Label + "</a></li>");
     }
 
     sectionHeight();
