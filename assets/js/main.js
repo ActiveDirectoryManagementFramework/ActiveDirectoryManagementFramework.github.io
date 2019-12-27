fc59e1512aebecca7d3fc66f20c49687009b88cf
Friedrich Weinmann <friedrich.weinmann@powershell-stuttgart.de>
2019-12-27 17:22:20 +0100

    full async operation

--- assets/js/main.js
+++ assets/js/main.js
@@ -19,19 +19,22 @@ var sectionHeight = function() {
   })
   
   $(function() {
+    var navigationBuilder = function(data) {
+        document.getElementById("backToParent").innerHtml = "<a href='" + results.backto.Link + "'>" + results.backto.Label + "</a>";
+        var i;
+        for (i = 0; i < results.fellows.length; i++) {
+            $("nav ul").append("<li class='tag-h2'><a href='" + results.fellows[i].Link + "'>" + results.fellows[i].Label + "</a></li>");
+        }
+    
+        sectionHeight();
+    }
     var results = null;
     $.ajax({
         type: 'GET',
         url: 'nav.json',
         dataType: 'json',
-        success: function(data) { results = data; },
-        async: false
+        success: navigationBuilder,
+        async: true
     });
-    document.getElementById("backToParent").innerHtml = "<a href='" + results.BackTo.Link + "'>" + results.BackTo.Label + "</a>";
-    var i;
-    for (i = 0; i < results.fellows.length; i++) {
-        $("nav ul").append("<li class='tag-h2'><a href='" + results.fellows[i].Link + "'>" + results.fellows[i].Label + "</a></li>");
-    }
-  
-    sectionHeight();
+    
   });
